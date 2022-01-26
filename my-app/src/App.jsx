6df0a0adc1dcd0c5b075207d121aacbb4c2a4ecb
Joe <wujoe1321@gmail.com>
2022-01-26 14:28:30 -0800

    change

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import ReactDOM from "react-dom";
+import React from "react";
+
+function Garage() {
+  return (
+    <div>
+      <h2>who lives in here</h2>
+      <car />;
+    </div>
+  );
+}
+
+// class Car extends React.Component {
+//   render() {
+//     return <h2>Hi, I am a Car!</h2>;
+//   }
+// }
+export default App;
