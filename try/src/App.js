42adf569b0fc4e07440a07da8099e1270ac8f8c6
Joe <wujoe1321@gmail.com>
2022-02-03 14:01:30 -0800

    fix changes

--- try/src/App.js
+++ try/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import SeasonDisplay from "./SeasonDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   window.navigator.geolocation.getCurrentPosition(function (position) {
+//     console.log(position);
+//   });
+//   return <div className="App">HI! THERE</div>;
+// }
+
+class App extends React.Component {
+  state = { lat: null, errMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error: {this.state.errMessage}</div>;
+    }
+    if (!this.state.errMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading...</div>;
+  }
 }
 
 export default App;
---
+++ try/src/SeasonDisplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SeasonDisplay = (props) => {
+  console.log(props);
+  return <div>okok</div>;
+};
+
+export default SeasonDisplay;
