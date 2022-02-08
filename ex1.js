ccff4afbe46d4d8baf25c27ee5dbba7f4ae9f297
Joe <wujoe1321@gmail.com>
2022-02-07 16:34:50 -0800

    add default

--- ex1.js
+++ ex1.js
@@ -1,1 +1,38 @@
-let arr = [1, 2];
+// const Greeters = [];
+// for (var i = 0; i < 10; i++) {
+//   Greeters.push(function () {
+//     return console.log(i);
+//   });
+// }
+// Greeters[1]();
+
+// let name = "window";
+
+// let A = {
+//   name: "A",
+//   sayHello: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// 输出A
+// A.sayHello();
+
+// let B = {
+//   name: "B",
+// };
+
+// A.sayHello.call(B); //输出B
+
+// let name = "window";
+
+// let A = {
+//   name: "A",
+//   sayHello: () => {
+//     console.log(this.name);
+//   },
+// };
+
+// A.sayHello();
+
+console.log(this);
--- try2/src/Components/Search.jsx
+++ try2/src/Components/Search.jsx
@@ -9,14 +9,19 @@ class SearchBar extends React.Component {
 
   state = { term: "" };
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
+              value={this.state.term}
               onChange={(event) => this.setState({ term: event })}
             />
           </div>
